6942675d6b46e4261d7cf1c403478671d7ba1f61
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-05-08 19:31:37 +0800

    layoutDone

--- app/src/main/java/assignment/Cards.java
+++ app/src/main/java/assignment/Cards.java
@@ -11,9 +11,9 @@ import java.awt.*;
 public class Cards extends JPanel {
     Cards(String rideName, String imgPath) {
         setBackground(Color.WHITE);
-        setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
         setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(10, 10));
+        setPreferredSize(new Dimension(100, 200));
 
         // Add character image
         JLabel imageLabel = new JLabel();
@@ -27,6 +27,7 @@ public class Cards extends JPanel {
         // Add character name
         JLabel nameLabel = new JLabel(rideName, JLabel.CENTER);
         nameLabel.setForeground(Color.BLACK);
+        nameLabel.setFont(new Font("SansSerif", Font.BOLD, 16));
         nameLabel.setBackground(Color.WHITE);
         nameLabel.setOpaque(true);
         add(nameLabel, BorderLayout.SOUTH);
--- app/src/main/java/assignment/RidesGrid.java
+++ app/src/main/java/assignment/RidesGrid.java
@@ -9,32 +9,53 @@ import java.awt.*;
 
 public class RidesGrid extends Button{
     JPanel createGrid() {
+        String[] rides = {"Steam Trains", 
+        "Chair lift cars", 
+        "Haunted House", 
+        "Ferris Wheel", 
+        "Hedge Maze", 
+        "Merry-go-round", 
+        "Wooden Roller Coaster",
+        "Wooden Wild Mouse",
+        "Junior Roller Coaster",
+        "Mini Train Coaster",
+        "Looping Roller Coaster",
+        "Suspended Swinging Cars",
+        "Twist",
+        "Pirate Ship",
+        "Go karts",
+        "Dinghy Slide",
+        "Rowing Boats",
+        };
+
+        String[] paths = {"/cardIcons/steamTrains.png",
+        "/cardIcons/flyover.png",
+        "/cardIcons/hauntedHouse.png",
+        "/cardIcons/ferrisWheel.png",
+        "/cardIcons/maze.png",
+        "/cardIcons/merryGoRound.png",
+        "/cardIcons/RCWood.png",
+        "/cardIcons/RCWoodMouse.png",
+        "/cardIcons/RCLdBird.png",
+        "/cardIcons/RCMiniTrain.png",
+        "/cardIcons/RCLoop.png",
+        "/cardIcons/RCSuspendSwing.png",
+        "/cardIcons/twist.png",
+        "/cardIcons/pirateShip.png",
+        "/cardIcons/goKarts.png",
+        "/cardIcons/wtrDingSlide.png",
+        "/cardIcons/wtrRowBoats.png",
+        };
+
         JPanel grid = new JPanel();
-        grid.setLayout(new GridLayout(0, 4, 1, 1));
+        grid.setLayout(new GridLayout(0, 4, 10, 10));
         grid.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        
-        Cards card = new Cards("Ride", "/cardIcons/maze.png");
-        card.setBackground(Color.WHITE);
-        grid.add(card);
-
-        Cards train = new Cards("ride", "/cardIcons/steamTrains.png");
-        card.setBackground(Color.WHITE);
-        grid.add(train);
-
-        Cards h = new Cards("ride", "/cardIcons/hauntedHouse.png");
-        card.setBackground(Color.WHITE);
-        grid.add(h);
-
-        Cards fr = new Cards("ride", "/cardIcons/ferrisWheel.png");
-        card.setBackground(Color.WHITE);
-        grid.add(fr);
-
-        Cards fly = new Cards("ride", "/cardIcons/flyover.png");
-        card.setBackground(Color.WHITE);
-        grid.add(fly);
-
-
+        for (int i = 0; i < rides.length; i++) {
+            Cards card = new Cards(rides[i], paths[i]);
+            card.setBackground(Color.WHITE);
+            grid.add(card);
+        }
 
         return grid;
     }
